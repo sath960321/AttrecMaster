8925ba475c9b7d3d83542286007af737523f0093
sath960321 <sath960321@gmail.com>
2023-02-04 17:40:13 -0500

    Vista Insertar Estudiantes + Arreglado de otros estilos

--- app/src/main/java/com/example/attrecmaster/AddEstudianteActivity.java
+++ app/src/main/java/com/example/attrecmaster/AddEstudianteActivity.java
@@ -3,31 +3,17 @@ package com.example.attrecmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+public class AddEstudianteActivity extends AppCompatActivity {
 
-public class ListadoEstudiantesActivity extends AppCompatActivity {
-    FloatingActionButton fabAddEstudiantes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listado_estudiantes);
-
-        fabAddEstudiantes = findViewById(R.id.fabAddEstudiantes);
-
-        fabAddEstudiantes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentAddEstudiante = new Intent(ListadoEstudiantesActivity.this, AddEstudianteActivity.class);
-                startActivity(intentAddEstudiante);
-            }
-        });
+        setContentView(R.layout.activity_add_estudiante);
     }
 
 
