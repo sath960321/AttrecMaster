f2283758731acfe3e975ec16328193c769d693e5
sath960321 <sath960321@gmail.com>
2023-02-05 20:57:13 -0500

    Clase de Control de Registro

---
+++ app/src/main/java/com/example/attrecmaster/clases/Registro_Control.java
@@ -0,0 +1,57 @@
+package com.example.attrecmaster.clases;
+
+public class Registro_Control {
+    private int id_registroControl;
+    private int id_registro;
+    private int id_estudiante;
+    private int id_evaluacion;
+    private int id_asistencia;
+
+    public Registro_Control() {
+        this.id_registroControl = id_registroControl;
+        this.id_registro = id_registro;
+        this.id_estudiante = id_estudiante;
+        this.id_evaluacion = id_evaluacion;
+        this.id_asistencia = id_asistencia;
+    }
+
+    public int getId_registroControl() {
+        return id_registroControl;
+    }
+
+    public void setId_registroControl(int id_registroControl) {
+        this.id_registroControl = id_registroControl;
+    }
+
+    public int getId_registro() {
+        return id_registro;
+    }
+
+    public void setId_registro(int id_registro) {
+        this.id_registro = id_registro;
+    }
+
+    public int getId_estudiante() {
+        return id_estudiante;
+    }
+
+    public void setId_estudiante(int id_estudiante) {
+        this.id_estudiante = id_estudiante;
+    }
+
+    public int getId_evaluacion() {
+        return id_evaluacion;
+    }
+
+    public void setId_evaluacion(int id_evaluacion) {
+        this.id_evaluacion = id_evaluacion;
+    }
+
+    public int getId_asistencia() {
+        return id_asistencia;
+    }
+
+    public void setId_asistencia(int id_asistencia) {
+        this.id_asistencia = id_asistencia;
+    }
+}
